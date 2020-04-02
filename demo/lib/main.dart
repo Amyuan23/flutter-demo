b71dd1f78a7b2003d2017f5e7300330da0e5929c
yuan xue <amyuan23@gmail.com>
2020-04-02 14:38:11 +0800

    hello

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -7,104 +7,37 @@ void main() => runApp(new MyApp()); // runApp挂载根
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //
+    // 用Material设计模式去包裹
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(Strings.hello),
+        title: 'Welcome to Flutter',
+        // Scaffold部分有页面顶部底部工具栏
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text(Strings.hello),
+          ),
+          body: new Column(
+            children: <Widget>[
+              Hello(),
+            ],
+          ),
         ),
-        body: new Column(
-          children: <Widget>[
-            // Fade(),
-            Center(
-              child: Text(
-                'kkki kk',
-                textDirection: TextDirection.ltr,
-                style: TextStyle(),
-              ),
-            ),
-            Container(
-              height: 0.5,
-              color: Color(0xFFEFF1F6),
-            ),
-          ],
-        ),
-      ),
-    );
+        theme: ThemeData.dark());
   }
 }
 
-class Fade extends StatefulWidget {
-  Fade({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _FadeState createState() => _FadeState();
-}
-
-class _FadeState extends State<Fade> with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-  CurvedAnimation curve;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-        duration: const Duration(milliseconds: 2000), vsync: this);
-    curve = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
-
+class Hello extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Container(
-              child: FadeTransition(
-                  opacity: curve,
-                  child: FlutterLogo(
-                    size: 100.0,
-                  )))),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Fade',
-        child: Icon(Icons.brush),
-        onPressed: () {
-          _controller.forward();
-        },
+    return Center(
+      child: Text(
+        'hello',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
       ),
     );
   }
 }
-
-// class Card extends StatelessWidget {
-//   Card({@required this.index,@required this.onPress});
-//   final index;
-//   final Function onPress;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return CardNew(
-//       child: Column(
-//         children: <Widget>[
-//         Text('card $index'),
-//         FlatButton(onPressed: this.onPress, child: const Text('press'))
-//       ],
-//       )
-//     );
-//   }
-// }
-
-// Card(index:1,onPress(){
-//   print('kkkk');
-// })
 
 // 静态字段系统
 class Strings {
