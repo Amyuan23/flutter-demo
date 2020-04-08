988a8bfdf17e1edbdf180726215d90867fee5a9c
yuan xue <amyuan23@gmail.com>
2020-04-08 17:52:47 +0800

    练习

--- lib/demo/basic.dart
+++ lib/demo/basic.dart
@@ -4,6 +4,15 @@ class BasicDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+            'https://resources.ninghao.org/images/childhood-in-a-picture.jpg',
+          ),
+          alignment: Alignment.topCenter,
+          fit: BoxFit.cover,
+        ),
+      ),
       child: Row(
         children: <Widget>[
           Container(
@@ -14,11 +23,11 @@ class BasicDemo extends StatelessWidget {
             width: 100, // con
             height: 100,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(10),
-              ),
-              color: Colors.green, // 如果要给设置radius,color有的话必须写里面
-            ),
+                // borderRadius: BorderRadius.all(
+                //   Radius.circular(10),
+                // ),
+                color: Colors.green, // 如果要给设置radius,color有的话必须写里面
+                shape: BoxShape.circle),
             child: Text(
               'data',
               textAlign: TextAlign.center,
